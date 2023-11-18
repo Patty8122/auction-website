3ff0f41a355438f8249a9d7168c438f60e7a453c
John Mitchell <jhmitchell@uchicago.edu>
2023-11-17 18:01:04 -0600

    init new-auction

--- auction-service/src/server.js
+++ auction-service/src/server.js
@@ -1,5 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+});
+
+const query = (text, params) => pool.query(text, params);
 
 dotenv.config();
 
@@ -10,6 +17,29 @@ app.use(express.json());
 app.get('/test', (req, res) => {
 	res.send('Auction service reached!');
 });
+
+app.post('/auctions', async (req, res) => {
+    const { itemId, startDateTime, endDateTime, startingPrice } = req.body;
+
+    try {
+        const result = await query('INSERT INTO auctions (item_id, start_date_time, end_date_time, starting_price) VALUES ($1, $2, $3, $4) RETURNING *', [itemId, startDateTime, endDateTime, startingPrice]);
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get('/auctions/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const result = await query('SELECT * FROM auctions WHERE auction_id = $1', [id]);
+        res.json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 
 // Start the server
 app.listen(PORT, () => {
