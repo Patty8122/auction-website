04535d2b6a7a5760542b1c29faa7e1afb1b38129
SOHAM GURJAR <sohamgurjar@SOHAMs-MacBook-Pro.local>
2023-12-03 16:11:08 -0600

    Email from Mediator

---
+++ scheduler/scheduler.py
@@ -0,0 +1,17 @@
+import time
+
+def main():
+    while True:
+        # Get all auctions data
+        
+        # Filter auctions that are STARTED and less than 1 hr left
+        
+        # Filter auctions that have status ENDING and have ended now
+        
+        # Call mediator to END AUCTION
+
+        # Sleep for 1 minute before polling the API again
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
