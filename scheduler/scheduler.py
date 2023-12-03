42c91cc309ff72317c3e44d65ac4900f4db70529
SOHAM GURJAR <sohamgurjar@SOHAMs-MacBook-Pro.local>
2023-12-03 17:43:51 -0600

    Scheduler

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,12 +1,18 @@
 import time
 
+UI_SERVICE_URL = "http://ui-service:3001"
+
 def main():
     while True:
         # Get all auctions data
         
+        # Filter auctions that are PENDING and past start time
+        
+        # Call mediator to START those AUCTIONs
+
         # Filter auctions that are STARTED and past end time
         print("Scheduler is up !")
-        # Call mediator to END AUCTION
+        # Call mediator to END those AUCTIONs
 
         # Sleep for 1 minute before polling the API again
         time.sleep(60)
