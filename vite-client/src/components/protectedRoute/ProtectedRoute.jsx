0438ce47b0b7b8757123d2e642911a3a9966ed8e
jhmitchell <mitchdog99@gmail.com>
2023-12-04 03:19:13 -0600

    admin route patched

--- vite-client/src/components/protectedRoute/ProtectedRoute.jsx
+++ vite-client/src/components/protectedRoute/ProtectedRoute.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useUser } from '@/hooks/user/useUser';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ adminOnly = false, children }) => {
     const { currentUser, isLoading } = useUser();
     
     if (isLoading) {
@@ -13,11 +13,18 @@ const ProtectedRoute = () => {
         return <Navigate to="/login" replace />;
     }
 
-    if (currentUser.user_type === 'admin') {
+    if (adminOnly && currentUser.user_type !== 'admin') {
+        return <Navigate to="/explore" replace />;
+    }
+
+    if (!adminOnly && currentUser.user_type === 'admin') {
+        console.log("Redirecting to admin page")
         return <Navigate to="/admin" replace />;
     }
 
-    return <Outlet />;
+    console.log("Rendering protected route")
+
+    return children;
 };
 
 export default ProtectedRoute;
